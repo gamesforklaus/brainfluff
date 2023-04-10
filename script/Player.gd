bc3d39e349a88c25d5ba2ec2b5998020f5d40306
gamesforklaus <nikolausfmarx@gmail.com>
2023-04-09 23:35:05 -0400

    more player updates

--- script/Player.gd
+++ script/Player.gd
@@ -21,16 +21,23 @@ class_name Puppet
 
 @export_category("Puppet")
 @export_subgroup("Attributes")
-@export var GROUND_SPEED  : float     = 27500.0
+@export var GROUND_SPEED  : float     = 17500.0
 @export var AIR_SPEED     : float     = 8000.0
 @export var JUMP_FORCE    : float     = 2500.0
-@export_subgroup("Nodes")
+@export_subgroup("Skeleton")
 @export var HEAD_BONE     : Bone2D
 @export var JAW_BONE      : Bone2D
-@export var FOOT_L        : RayCast2D
-@export var FOOT_R        : RayCast2D
+@export var FOOT_L        : Marker2D
+@export var FOOT_R        : Marker2D
 @export var ARM_L         : StretchBone
 @export var ARM_R         : StretchBone
+@export_subgroup("Timers")
+@export var T_BUFFER      : Timer
+@export var T_COYOTE      : Timer
+
+# Leg animation variables
+const LEG_RADIUS = 19.5
+var l_default_pos = [Vector2.ZERO, Vector2.ZERO]
 
 # Player states
 enum pS {
@@ -45,14 +52,21 @@ var trans = {
 	pS.IDLE : [pS.MOVE, pS.JUMP, pS.FALL],
 	pS.MOVE : [pS.IDLE, pS.JUMP, pS.FALL],
 	pS.JUMP : [pS.FALL],
-	pS.FALL : [pS.IDLE]
+	pS.FALL : [pS.IDLE, pS.JUMP]
 }
 
 # Current status variables
 var floor_normal : Vector2 = Vector2.UP
 var p_state      : pS      = pS.IDLE
-var velocity     : Vector2 = Vector2.ZERO
+var velocity     : float   = 0.0
 var grabbing     : bool    = false
+var jumping      : bool    = false
+var buffer       : bool    = false
+
+func _ready() -> void:
+	# Get initial positions of feet
+	l_default_pos[0] = FOOT_L.position
+	l_default_pos[1] = FOOT_R.position
 
 func _integrate_forces(state : PhysicsDirectBodyState2D) -> void:
 	# Declare variables
@@ -68,6 +82,13 @@ func _integrate_forces(state : PhysicsDirectBodyState2D) -> void:
 				change_state(pS.MOVE)
 			elif on_ground and Input.is_action_just_pressed("jump"):
 				change_state(pS.JUMP)
+			
+			# Movement
+			velocity = lerp(
+				velocity,
+				0.0,
+				0.1
+			)
 		pS.MOVE:
 			# Transitions
 			if !move_direction.x:
@@ -78,11 +99,51 @@ func _integrate_forces(state : PhysicsDirectBodyState2D) -> void:
 				change_state(pS.JUMP)
 			
 			# Movement
-			state.linear_velocity.x = lerp(
-				state.linear_velocity.x,
+			velocity = lerp(
+				velocity,
 				move_direction.x * GROUND_SPEED * delta,
 				0.1
 			)
+		pS.JUMP:
+			# Transitions
+			if state.linear_velocity.y > 0:
+				change_state(pS.FALL)
+			
+			# Movement
+			velocity = lerp(
+				velocity,
+				move_direction.x * AIR_SPEED * delta,
+				0.1
+			)
+		pS.FALL:
+			# Transitions
+			if Input.is_action_just_pressed("jump"):
+				buffer = true
+				T_BUFFER.start()
+			if on_ground:
+				if buffer:
+					change_state(pS.JUMP)
+				else:
+					change_state(pS.IDLE)
+			
+			# Movement
+			velocity = lerp(
+				velocity,
+				move_direction.x * AIR_SPEED * delta,
+				0.1
+			)
+	
+	# Run animation calls
+	animate_legs()
+	
+	# Apply velocity
+	state.linear_velocity.x = velocity
+
+# Fired when input buffer times out
+func _buffer_timeout():
+	buffer = false
+
+
 # FUNCTION
 #-------------------------------------------------------------------------------
 
@@ -98,13 +159,45 @@ func initialize_state(target : pS) -> void:
 	p_state = target
 	match target:
 		pS.IDLE:
+			jumping = false
 			linear_velocity.x = 0
 		pS.JUMP:
+			jumping = true
 			apply_central_impulse(floor_normal * JUMP_FORCE)
 		pS.FALL:
 			return
 		_:
 			return
+
+# Animates legs by using sin functions
+func animate_legs() -> void:
+	# Get movement vector
+	var movement_vec = get_movement_vector()
+	
+	if movement_vec.x:
+		# Set leg marker positions
+		FOOT_L.position.x = lerp(
+			FOOT_L.position.x,
+			l_default_pos[0].x + sin(Time.get_ticks_msec() / 80) * LEG_RADIUS,
+			0.1
+		)
+		FOOT_R.position.x = lerp(
+			FOOT_R.position.x,
+			l_default_pos[1].x + -sin(Time.get_ticks_msec() / 80) * LEG_RADIUS,
+			0.1
+		)
+	else:
+		# Set leg marker positions
+		FOOT_L.position.x = lerp(
+			FOOT_L.position.x,
+			l_default_pos[0].x,
+			0.2
+		)
+		FOOT_R.position.x = lerp(
+			FOOT_R.position.x,
+			l_default_pos[1].x,
+			0.2
+		)
 
 # Returns a movement vector
 func get_movement_vector() -> Vector2:
